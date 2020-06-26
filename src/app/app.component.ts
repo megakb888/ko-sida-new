c84093d09e6895de614c22c0eaeaa4be619932f5
Kristoffer Bengtsson <krbe@nao.local>
2020-06-26 14:45:27 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ko-sida-new';
+  name = "Angular";
+
+  @ViewChild("henrik", { static: false }) henrik;
+  @ViewChild("vision", { static: false }) vision;
+  @ViewChild("process", { static: false }) process;
+  @ViewChild("info", { static: false }) info;
+  @ViewChild("levererar", { static: false }) levererar;
+  @ViewChild("team", { static: false }) team;
+  @ViewChild("kontakt", { static: false }) kontakt;
+
+  navigate(element: string) {
+    this[element].nativeElement.scrollIntoView({ behavior: "smooth" });
+  }
 }
